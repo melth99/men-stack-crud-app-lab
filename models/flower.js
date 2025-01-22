8679a9268c34ee5cc2992f6b5327f417d90de5dc
mel-th <meleyth@gmail.com>
2025-01-21 23:23:11 -0500

    working on updating flower

--- models/flower.js
+++ models/flower.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 
 const flowerSchema = new mongoose.Schema({
-  symmetry: String, //radial or bilateral
+
+  species: String,
+  commonName: String,
   isPerfect: Boolean, //containing male & female organs
-  lifeCycle: Number, //1 = annual 2= biennals, 3 = 2+ years
-  vascular: Boolean, //containing vascular tissue
   image: String, //image of flower
 
 });
 
-const flower = mongoose.model('flower', flowerSchema);
+const flowers = mongoose.model('flowers', flowerSchema);
 
-module.exports = flower;
+module.exports = flowers;
+
+
--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
+const morgan = require('morgan')
 dotenv.config() //get variables from our env file
-
+const methodOverride = require('method-override')
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan("dev")); //new
+app.use(methodOverride("_method")); // 
 //verifying server connection
 app.listen(3000, () => {
-console.log("listening on port 3000")})
-
+    console.log("listening on port 3000")
+})
 
 
 //import model to talk to the db
@@ -17,19 +21,55 @@ const FlowerModel = require('./models/flower')
 mongoose.connect(process.env.MONGODB_URI)
 
 //event listener for "connected" event
-mongoose.connection.on('connected', function() {
+mongoose.connection.on('connected', function () {
     console.log('Express has established a connection with mongoDB <3')
 })
 
 //middleware to process form requests
 //routes can access req.body to get content about our flowers
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 
-app.get("/", (req ,res) => {
+app.get("/", (req, res) => {
     res.render("index.ejs")
 })
+//action - index all plants list! - WORKS
+app.get('/flowers', async (req,res) => {
+    const bouquet = await FlowerModel.find({})
+    console.log(bouquet) //all flowers
+    res.render("flowers/index.ejs",{flowerDoc: bouquet} )
+})
 
+app.get('/flowers', (req,res) => {
+    res.render("flowers/show.ejs")
+})  
 //render always look in views folder for ejs files
-app.get("/roses", (req,res)=>{
-    res.render("index.ejs")
+app.get("/flowers/new", (req, res) => {
+    res.render("flowers/new.ejs")
+})
+//create functionality
+app.post('/flowers', async function (req, res){
+    console.log(req.body, "body of request")
+    req.body.isPerfect = !!req.body.isPerfect
+    flowerDoc = await FlowerModel.create(req.body)
+    console.log(flowerDoc)
+    res.redirect("/flowers")
+    })
+
+//SHOW specific ID
+app.get('/flowers/:flowersId', async (req,res) => {
+  //  http://localhost:3000/flowers/?flowersId:678f03bb5a62912de0a885ac
+    console.log(req.params.flowersId)
+    const flowerDoc = await FlowerModel.findById(req.params.flowersId)
+    console.log(flowerDoc)
+    res.render('flowers/show.ejs', {flowerDoc: flowerDoc})
+})
+
+
+//DELETE
+
+app.delete('/flowers/:flowersId', async function(req, res){
+	const deletedFlower = await FlowerModel.findByIdAndDelete(req.params.fruitId)
+	res.redirect('/flowers')
+    
+
 })
